d5c9637c8a65cac45572c99ee2e5f1340a734674
uladzislau <uladzislau.zayats@amsterdam-standard.pl>
2019-01-25 11:29:40 +0100

    PTWO-216 Dialogs Confirmation/Forms

--- typings/exmg-cms-styles/exmg-cms-dialog-styles.d.ts
+++ typings/exmg-cms-styles/exmg-cms-dialog-styles.d.ts
@@ -1,6 +1,3 @@
 declare module '@exmg/exmg-cms-styles/exmg-cms-dialog-styles.js' {
     export const sharedDialogStyles: HTMLTemplateElement;
 }
-declare module '@exmg/exmg-cms-styles/exmg-cms-button-styles.js' {
-    export const sharedButtonStyles: HTMLTemplateElement;
-}
