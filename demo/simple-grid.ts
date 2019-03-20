10a21d960492cb988d511dc559a5f4108f044455
pgoc <pawel.goc@amsterdam-standard.pl>
2019-03-20 12:05:23 +0100

    sortable rows grid

--- demo/simple-grid.ts
+++ demo/simple-grid.ts
@@ -1,23 +1,32 @@
 import {LitElement, html, customElement, TemplateResult, property} from 'lit-element';
+import {repeat} from 'lit-html/directives/repeat';
 import '../src/table/exmg-grid';
 import '../src/table/exmg-grid-paging';
 import {style as tableStyles} from '../src/table/exmg-grid-styles';
 
 type Income = {
+  id: number;
   month: string;
   amount: number;
+  year: number;
 };
 
-const generateRows = (): Income[] => {
+const generateRows = (length: number = 50, startId: number = 1): Income[] => {
   const source: Income[] = [
-    {month: 'January', amount: 1},
-    {month: 'February', amount: 2},
-    {month: 'March', amount: 3},
+    {id: 1, month: 'January', amount: 1, year: 2000},
+    {id: 2, month: 'February', amount: 2, year: 2000},
+    {id: 3, month: 'March', amount: 3, year: 2000},
   ];
 
   const rows: Income[] = [];
-  while (rows.length < 1000) {
-    rows.push(...source);
+  let id: number = startId;
+  while (rows.length < length) {
+    id = id + 1;
+    const items = source.map(it => {
+      id = id + 1;
+      return {...it, id};
+    });
+    rows.push(...items);
   }
 
   return rows;
@@ -28,7 +37,7 @@ export class DemoSimpleGridTable extends LitElement {
   static styles = [tableStyles];
 
   @property({type: Object})
-  items: Income[] = generateRows();
+  items: Income[] = generateRows(20);
 
   @property({type: Number})
   private pageIndex: number = 0;
@@ -36,13 +45,81 @@ export class DemoSimpleGridTable extends LitElement {
   @property({type: Number})
   private pageSize: number = 10;
 
+  @property({type: Object})
+  private hiddenColumns: Record<string, string> = {};
+
+  @property({type: Boolean})
+  private selectable: boolean = true;
+
   private deleteItems(): void {
     // do nothing
   }
 
+  private toggleMonthColumn(event: Event) {
+    event.preventDefault();
+    const {month, ...rest} = this.hiddenColumns;
+    this.hiddenColumns = month ? {...rest} : {...rest, month: 'month'};
+  }
+
+  private toggleYearColumn(event: Event) {
+    event.preventDefault();
+    const {year, ...rest} = this.hiddenColumns;
+    this.hiddenColumns = year ? {...rest} : {...rest, year: 'year'};
+  }
+
+  private toggleSelectable(event: Event) {
+    event.preventDefault();
+    this.selectable = !this.selectable;
+  }
+
+  private loadMore(event: Event): void {
+    event.preventDefault();
+    this.items = [...this.items, ...generateRows(10, this.items.length + 1)];
+  }
+
+  private updateItems(event: CustomEvent): void {
+    console.log('on update items', event);
+    this.items = [...event.detail.items];
+  }
+
+  private renderTableBody() {
+    return repeat(
+      this.items,
+      ({id}) => id,
+      (i) => {
+        return html`
+          <tr data-row-key="${i.id}">
+            <td class="handle"><span class="row-sortable-handler">MOVE</span></td>
+            <td><paper-checkbox></paper-checkbox></td>
+            <td>${i.month}</td>
+            <td>${i.year}</td>
+            <td>${i.amount}</td>
+            <td></td>
+          </tr>
+          <tr class="row-detail" style="display: none;">
+            <td colspan="3">Expandable</td>
+          </tr>
+        `;
+      }
+    );
+  }
+
   protected render(): TemplateResult | void {
     return html`
-      <exmg-grid>
+      <div>
+        <button @click="${this.toggleMonthColumn}">Hide Month</button>
+        <button @click="${this.toggleYearColumn}">Hide Year</button>
+        <button @click="${this.toggleSelectable}">Toggle selectable</button>
+        <button @click="${this.loadMore}">Load More</button>
+      </div>
+      <exmg-grid
+        .items=${this.items}
+        .hiddenColumnNames="${this.hiddenColumns}"
+        ?selectable="${false}"
+        ?sortable="${false}"
+        ?rows-sortable="${true}"
+        @exmg-grid-update-items="${this.updateItems}"
+      >
         <table role="grid" aria-labelledby="grid1Label" class="data">
           <thead>
            <tr>
@@ -59,18 +136,14 @@ export class DemoSimpleGridTable extends LitElement {
            </tr>
            <tr class="columns">
              <th></th>
-             <th sort="month">Month</th>
+             <th></th>
+             <th data-column-key="month" data-sort>Month</th>
+             <th data-column-key="year" data-sort>Year</th>
              <th>Income</th>
            </tr>
           </thead>
-          <tbody>
-           ${this.items.map(i => html`
-                 <tr>
-                   <td><paper-checkbox></paper-checkbox></td>
-                   <td>${i.month}</td>
-                   <td>${i.amount}</td>
-                 </tr>
-               `)}
+          <tbody class="data">
+            ${this.renderTableBody()}
           </tbody>
           <tfoot>
            <tr>
@@ -88,5 +161,5 @@ export class DemoSimpleGridTable extends LitElement {
         </table>
       </exmg-grid>
 `;
-      }
+  }
 }
--- src/table/exmg-grid.ts
+++ src/table/exmg-grid.ts
@@ -1,9 +1,50 @@
 import {property, customElement, html, LitElement} from 'lit-element';
-// import {repeat} from 'lit-html/directives/repeat';
+import {cache} from 'lit-html/directives/cache';
+import '@exmg/exmg-sortable';
 import {style as exmgGridTableStyles} from './exmg-grid-styles';
 
+type GenericPropertyValues<T, V = unknown> = Map<T, V>;
+type Props = Exclude<keyof ExmgGrid, number | Symbol>;
+
+type SmartPropertyValue = GenericPropertyValues<Props>;
+
+const checkCheckbox = (checkboxElement: HTMLInputElement) => {
+    checkboxElement.setAttribute('checked', 'checked');
+    checkboxElement.checked = true;
+};
+
+const uncheckCheckbox = (checkboxElement: HTMLInputElement) => {
+  checkboxElement.removeAttribute('checked');
+  checkboxElement.checked = false;
+};
+
+/**
+ * Questions:
+ * - shall we keep selected items on page changed ?
+ * - shall we keep selected items on filter changed ? probably not
+ * Required
+ * <table>
+ *     <thead>
+ *         <tr class="columns"><th /><tr/>
+ *     </thead>
+ *     <tbody class="data"><tbody>
+ * </table>
+
+ * TR -> TH - must have class ".columns"
+ * TH:
+ * [F] column manager
+ * - data-column-key
+ * [F]sortable:
+ *  - data-sort
+ *  - data-sort-direction?
+ *  [F] ExpandedRow: Must have class ".row-detail"
+ *  [F] Row sortable
+ *  - hast to handle event @exmg-grid-update-items - event return updated order of items - consumer needs to reiterate tbody
+ *  - handler element should has class `row-sortable-handler`
+ *  - element should use repeat function - it matter in terms of performance and will persist node's state properly like selected row
+ */
 @customElement('exmg-grid')
-export class ExmgRadioGroup extends LitElement {
+export class ExmgGrid extends LitElement {
   static styles = [
     exmgGridTableStyles,
   ];
@@ -11,114 +52,233 @@ export class ExmgRadioGroup extends LitElement {
   @property({type: Object})
   items: object[] = [];
 
-  @property({type: Boolean})
-  sortable: boolean = true;
+  @property({type: Boolean, reflect: true})
+  sortable: boolean = false;
 
-  @property({type: Boolean})
-  selectable: boolean = true;
+  /**
+   * Feature that turn on sort by column
+   */
+  @property({type: Boolean, reflect: true})
+  selectable: boolean = false;
+
+  /**
+   * Feature that allow sort rows
+   * If table table has turn on feature `selectable` then it takes precedence and `rowSelectable` will be ignored
+   */
+  @property({type: Boolean, reflect: true, attribute: 'rows-sortable'})
+  rowsSelectable: boolean = false;
+
+  /**
+   * Map of column names that should be hidden
+   */
+  @property({type: Object})
+  hiddenColumnNames: Record<string, string> = {};
+
+  @property({type: Object})
+  private tableBody?: HTMLTableSectionElement;
+
+  private table?: HTMLTableElement;
 
   private selectedRows: HTMLTableRowElement[] = [];
 
   private allCheckbox?: HTMLInputElement;
 
-  protected firstUpdated(): void {
-    const table = this.shadowRoot!.host.querySelector<HTMLTableElement>('table')!;
+  private componentReady: boolean = false;
+
+  private getTable(): HTMLTableElement {
+    if (!this.table) {
+      throw new Error(`Element table not found. Slot hast to define <table>`);
+    }
+    return this.table!;
+  }
+
+  private getTableBody(): HTMLTableSectionElement {
+    if (!this.tableBody) {
+      throw new Error(`Element tbody not found. Slot hast to define <tbody class="data">`);
+    }
+
+    return this.tableBody!;
+  }
+
+  private getColumns(selector: string = 'th'): NodeListOf<HTMLTableHeaderCellElement> {
+    return this.getTable().querySelectorAll<HTMLTableHeaderCellElement>(`.columns ${selector}`);
+  }
+
+  private canSortRows(): boolean {
+    return !this.sortable && this.rowsSelectable;
+  }
+
+  private orderChange(e: CustomEvent): void {
+    console.log('order change', e.detail);
+    setTimeout(() => {
+      console.log('order change', e.detail);
+      const {sourceIndex, targetIndex} = e.detail;
+      const items = [...this.items];
+      const movedElement = items[sourceIndex];
+
+      items.splice(sourceIndex, 1);
+      items.splice(targetIndex, 0, movedElement);
+
+      this.dispatchEvent(new CustomEvent('exmg-grid-update-items', {composed: true, bubbles: true, detail: {items}}));
+    }, 0);
+  }
+
+  private updateColumnVisibility(previousHiddenColumnNames: Record<string, string> = {}): void {
+    this.getColumns().forEach((column, index) => {
+      const columnKey = column.getAttribute('data-column-key');
+      if (columnKey && previousHiddenColumnNames[columnKey] !== this.hiddenColumnNames[columnKey]) {
+        const nextDisplayValue = this.hiddenColumnNames[columnKey] ? 'none' : 'table-cell';
+        column.style.display = nextDisplayValue;
+        this.getTable().querySelectorAll<HTMLInputElement>(`tbody.data tr:not(.row-detail) td:nth-child(${index + 1})`)
+          .forEach(cell => {
+            cell.style.display = nextDisplayValue;
+          });
+      }
+    });
+  }
+
+  private turnOnSortable(): void {
+    this.getColumns('th[data-sort]').forEach((column  => {
+      const columnId = column.getAttribute('data-sort') || column.getAttribute('data-column-key');
+      column.addEventListener('click', () => {
+        const columnSortDirection = column.getAttribute('data-sort-direction');
+        const nextSortDirection = columnSortDirection === 'ASC' ? 'DESC' : columnSortDirection === 'DESC' ? '' : 'ASC';
+        column.setAttribute('data-sort-direction', nextSortDirection);
+        console.log('sortable clicked', columnId, nextSortDirection);
+        // fire event!
+      });
+    }));
+  }
+
+  private turnOnSelectable(): void {
+    const checkboxTemplate = html`<input class="selection-checkbox" type="checkbox" />`;
+    const rowColumns = this.componentReady ? null : this.getTable().querySelector('tr.columns');
+    if (rowColumns) {
+      const th = document.createElement('th');
+      th.setAttribute('class', 'selectable-cell');
+      th.innerHTML = checkboxTemplate.getHTML();
+      rowColumns.prepend(th);
+      this.allCheckbox = th.querySelector<HTMLInputElement>('.selection-checkbox')!;
+      this.allCheckbox.addEventListener('change', () => {
+        if (this.allCheckbox!.checked) {
+          this.getTableBody().querySelectorAll<HTMLTableRowElement>('tr:not(.row-details)').forEach(row => {
+            row.setAttribute('data-selected', '');
+            this.selectedRows.push(row);
+          });
+          this.getTableBody().querySelectorAll<HTMLInputElement>('.selection-checkbox').forEach(checkbox => {
+            checkCheckbox(checkbox);
+          });
+        } else {
+          this.selectedRows = [];
+          this.getTableBody().querySelectorAll<HTMLTableRowElement>('tr[data-selected]').forEach(row => {
+            row.removeAttribute('data-selected');
+          });
+          this.getTableBody().querySelectorAll<HTMLInputElement>('.selection-checkbox').forEach(checkbox => {
+            uncheckCheckbox(checkbox);
+          });
+        }
+      });
+    }
+
+    this.getTableBody().querySelectorAll<HTMLTableRowElement>('tr:not([data-is-selectable])')!.forEach((row => {
+      const td = document.createElement('td');
+      td.setAttribute('class', 'selectable-cell');
+      td.innerHTML = checkboxTemplate.getHTML();
+      row.prepend(td);
+      row.setAttribute('data-is-selectable', '');
+
+      row.addEventListener('click', (e: Event) => {
+        const index = this.selectedRows.indexOf(row);
+        const isRowAlreadySelected = index > -1;
+        const checkbox = row.querySelector<HTMLInputElement>('.selection-checkbox');
+        if (isRowAlreadySelected) {
+          row.removeAttribute('data-selected');
+          this.selectedRows.splice(index, 1);
+        } else {
+          row.setAttribute('data-selected', '');
+          this.selectedRows.push(row);
+        }
+
+        if (checkbox && checkbox !== e.target) {
+          if (isRowAlreadySelected) {
+            uncheckCheckbox(checkbox);
+          } else {
+            checkCheckbox(checkbox);
+          }
+        }
+
+        this.updateSelectAllCheckbox();
+        console.log('row clicked', isRowAlreadySelected, this.selectedRows);
+      });
+    }));
+  }
+
+  private updateSelectAllCheckbox(): void {
+    const selectedRowsCount = this.selectedRows.length;
+    if (this.allCheckbox) {
+      if (!this.allCheckbox.checked && selectedRowsCount === this.getTableBody().querySelectorAll('tr').length) {
+        checkCheckbox(this.allCheckbox);
+      } else if (this.allCheckbox.checked && selectedRowsCount < this.getTableBody().querySelectorAll('tr').length) {
+        uncheckCheckbox(this.allCheckbox);
+      }
+    }
+  }
+
+  protected async firstUpdated(changedProps: SmartPropertyValue): Promise<void> {
+    const debug = Array.from(changedProps.keys()).map(key => ({key, oldV: changedProps.get(key), newV: this[key]}));
+    console.log('FIRST changedProps', changedProps, debug);
+    this.table = this.shadowRoot!.host.querySelector<HTMLTableElement>('table')!;
+    this.tableBody = this.getTable().querySelector<HTMLTableSectionElement>('tbody.data')!;
+
     if (this.sortable) {
-      table.querySelectorAll<HTMLTableHeaderCellElement>('th[sort]')!.forEach((column  => {
-        const columnId = column.getAttribute('sort');
-        console.log('sortable', column);
-        column.addEventListener('click', () => {
-          const columnSortDirection = column.getAttribute('data-sort-direction');
-          const nextSortDirection = columnSortDirection === 'ASC' ? 'DESC' : columnSortDirection === 'DESC' ? '' : 'ASC';
-          column.setAttribute('data-sort-direction', nextSortDirection);
-          console.log('sortable clicked', columnId, nextSortDirection);
-          // fire event!
-        });
-      }));
+      this.turnOnSortable();
     }
 
     if (this.selectable) {
-      const checkboxTemplate = html`<input class="selection-checkbox" type="checkbox" />`;
-      const rowColumns = table.querySelector('thead tr:last-child');
-      if (rowColumns) {
-        const th = document.createElement('th');
-        th.innerHTML = checkboxTemplate.getHTML();
-        rowColumns.prepend(th);
-        this.allCheckbox = th.querySelector<HTMLInputElement>('.selection-checkbox')!;
-        this.allCheckbox.addEventListener('change', () => {
-          if (this.allCheckbox!.checked) {
-            table.querySelectorAll<HTMLTableRowElement>('tbody tr').forEach(row => {
-              row.setAttribute('data-selected', '');
-              this.selectedRows.push(row);
-            });
-            table.querySelectorAll<HTMLInputElement>('tbody .selection-checkbox').forEach(checkbox => {
-              checkbox.setAttribute('checked', 'checked');
-              checkbox.checked = true;
-            });
-          } else {
-            this.selectedRows = [];
-            table.querySelectorAll<HTMLTableRowElement>('tbody tr').forEach(row => {
-              row.removeAttribute('data-selected');
-            });
-            table.querySelectorAll<HTMLInputElement>('tbody .selection-checkbox').forEach(checkbox => {
-              checkbox.removeAttribute('checked');
-              checkbox.checked = false;
-            });
-          }
-        });
+      this.turnOnSelectable();
+    }
+
+    await this.updateComplete;
+    this.componentReady = true;
+  }
+
+  protected updated(changedProps: SmartPropertyValue): void {
+    console.log('changedProps', changedProps);
+    if (changedProps.has('hiddenColumnNames') || changedProps.has('items')) {
+      this.updateColumnVisibility(changedProps.get('hiddenColumnNames') as Record<string, string>);
+    }
+
+    if (this.componentReady && changedProps.has('items')) {
+      console.log('turn on sortable???');
+      if (this.selectable) {
+        this.turnOnSelectable();
+        this.updateSelectAllCheckbox();
       }
-
-      console.log('last row columns', rowColumns);
-      table.querySelectorAll<HTMLTableRowElement>('tbody tr')!.forEach((row => {
-        console.log('row', row);
-        const td = document.createElement('td');
-        td.innerHTML = checkboxTemplate.getHTML();
-        row.prepend(td);
-
-        row.addEventListener('click', (e: Event) => {
-          const index = this.selectedRows.indexOf(row);
-          const isRowAlreadySelected = index > -1;
-          const checkbox = row.querySelector<HTMLInputElement>('.selection-checkbox');
-          if (isRowAlreadySelected) {
-            row.removeAttribute('data-selected');
-            this.selectedRows.splice(index, 1);
-          } else {
-            row.setAttribute('data-selected', '');
-            this.selectedRows.push(row);
-          }
-
-          if (checkbox && checkbox !== e.target) {
-            if (isRowAlreadySelected) {
-              checkbox.removeAttribute('checked');
-              checkbox.checked = false;
-            } else {
-              checkbox.setAttribute('checked', 'checked');
-              checkbox.checked = true;
-            }
-          }
-
-          if (this.allCheckbox && !this.allCheckbox.checked && this.selectedRows.length === table.querySelectorAll('tbody tr').length) {
-            this.allCheckbox.setAttribute('checked', 'checked');
-            this.allCheckbox.checked = true;
-          } else if (this.allCheckbox && this.allCheckbox.checked && this.selectedRows.length < table.querySelectorAll('tbody tr').length) {
-            this.allCheckbox.removeAttribute('checked');
-            this.allCheckbox.checked = false;
-          }
-          console.log('row clicked', isRowAlreadySelected, this.selectedRows);
-        });
-      }));
     }
   }
 
-  protected updated(): void {
-
+  private renderWithoutSortable() {
+    return html`<slot></slot>`;
   }
 
-  render() {
+  private renderWithSortable() {
     return html`
-      <div>
-        Here is table
-        <slot></slot>
+        <exmg-sortable
+          orientation="${'vertical'}"
+          item-selector="tbody.data tr:not(.row-detail)"
+          handle-selector=".row-sortable-handler"
+          .sortableHostNode="${this.tableBody}"
+          @dom-order-change="${this.orderChange}"
+        >
+          <slot></slot>
+        </exmg-sortable>
+    `;
+  }
+
+  protected render() {
+    return html`
+      <div class="table-container">
+        ${cache(this.canSortRows() ? this.renderWithSortable() : this.renderWithoutSortable())}
       </div>
     `;
   }
