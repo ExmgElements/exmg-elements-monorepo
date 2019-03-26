2c97435359786b0af7d38cee6f91498ae009520f
uladzislau <uladzislau.zayats@amsterdam-standard.pl>
2019-03-26 17:02:00 +0100

    updated dependencies & config, renamed event name

--- exmg-copy-to-clipboard.ts
+++ exmg-copy-to-clipboard.ts
@@ -106,7 +106,7 @@ export class ExmgCopyToClipboard extends LitElement {
 
     try {
       document.execCommand('copy');
-      this.dispatchEvent(new CustomEvent('copy-successful', {
+      this.dispatchEvent(new CustomEvent('exmg-copy-to-clipboard-copied', {
         detail: this.value,
         bubbles: this.bubbles,
         composed: true,
