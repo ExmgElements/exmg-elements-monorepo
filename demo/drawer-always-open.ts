26c92b9b77dcf997e41f1c0bc4d3a87b21684d13
uladzislau <uladzislau.zayats@amsterdam-standard.pl>
2019-04-02 12:09:49 +0200

    PTWO-408 Drawer form

--- demo/drawer-always-open.ts
+++ demo/drawer-always-open.ts
@@ -25,6 +25,7 @@ export class DrawerAlwaysOpen extends LitElement {
   static styles = style;
 
   render () {
+    console.log(this.open);
     //language=html
     return html`
       <exmg-form-drawer hasheader="" type="modal" ?open="${this.open}" @MDCDrawer:closed="${this.onDrawerClosed}">
@@ -32,50 +33,50 @@ export class DrawerAlwaysOpen extends LitElement {
         <span slot="subtitle">subtitle</span>
         <div class="drawer-content">
         
-          <nav class="mdc-list">
-            <a class="mdc-list-item mdc-list-item--activated" href="#" aria-current="page">
-              <mwc-icon class="mdc-list-item__graphic">library_books</mwc-icon>
-              <span class="mdc-list-item__text">Broadcast Overview</span>
-            </a>
-            <a class="mdc-list-item" href="#">
-              <mwc-icon class="mdc-list-item__graphic">art_track</mwc-icon>
-              <span class="mdc-list-item__text">Program</span>
-            </a>
-            <a class="mdc-list-item" href="#">
-            <mwc-icon class="mdc-list-item__graphic">recent_actors</mwc-icon>
-              <span class="mdc-list-item__text">Presenter View</span>
-            </a>
-            <a class="mdc-list-item" href="#">
-              <mwc-icon class="mdc-list-item__graphic">videocam</mwc-icon>
-              <span class="mdc-list-item__text">Video</span>
-            </a>
-            <a class="mdc-list-item" href="#">
-              <mwc-icon class="mdc-list-item__graphic">trending_up</mwc-icon>
-              <span class="mdc-list-item__text">Broadcast Analytics</span>
-            </a>
-          
-            <hr class="mdc-list-divider">
-            <h6 class="mdc-list-group__subheader">Users</h6>
-            <a class="mdc-list-item" href="#">
-              <mwc-icon class="mdc-list-item__graphic">people</mwc-icon>
-              <span class="mdc-list-item__text">User Overview</span>
-            </a>
-            <a class="mdc-list-item" href="#">
-              <mwc-icon class="mdc-list-item__graphic">notifications_active</mwc-icon>
-              <span class="mdc-list-item__text">Notifications</span>
-            </a>
-            <a class="mdc-list-item" href="#">
-              <mwc-icon class="mdc-list-item__graphic">security</mwc-icon>
-              <span class="mdc-list-item__text">Roles and Rights</span>
-            </a>
-          
-            <hr class="mdc-list-divider">
-            <h6 class="mdc-list-group__subheader">Gamification</h6>
-            <a class="mdc-list-item" href="#">
-              <mwc-icon class="mdc-list-item__graphic">people</mwc-icon>
-              <span class="mdc-list-item__text">Leaderboards</span>
-            </a>
-          </nav>
+<nav class="mdc-list">
+  <a class="mdc-list-item mdc-list-item--activated" href="#" aria-current="page">
+    <mwc-icon class="mdc-list-item__graphic">library_books</mwc-icon>
+    <span class="mdc-list-item__text">Broadcast Overview</span>
+  </a>
+  <a class="mdc-list-item" href="#">
+    <mwc-icon class="mdc-list-item__graphic">art_track</mwc-icon>
+    <span class="mdc-list-item__text">Program</span>
+  </a>
+  <a class="mdc-list-item" href="#">
+  <mwc-icon class="mdc-list-item__graphic">recent_actors</mwc-icon>
+    <span class="mdc-list-item__text">Presenter View</span>
+  </a>
+  <a class="mdc-list-item" href="#">
+    <mwc-icon class="mdc-list-item__graphic">videocam</mwc-icon>
+    <span class="mdc-list-item__text">Video</span>
+  </a>
+  <a class="mdc-list-item" href="#">
+    <mwc-icon class="mdc-list-item__graphic">trending_up</mwc-icon>
+    <span class="mdc-list-item__text">Broadcast Analytics</span>
+  </a>
+
+  <hr class="mdc-list-divider">
+  <h6 class="mdc-list-group__subheader">Users</h6>
+  <a class="mdc-list-item" href="#">
+    <mwc-icon class="mdc-list-item__graphic">people</mwc-icon>
+    <span class="mdc-list-item__text">User Overview</span>
+  </a>
+  <a class="mdc-list-item" href="#">
+    <mwc-icon class="mdc-list-item__graphic">notifications_active</mwc-icon>
+    <span class="mdc-list-item__text">Notifications</span>
+  </a>
+  <a class="mdc-list-item" href="#">
+    <mwc-icon class="mdc-list-item__graphic">security</mwc-icon>
+    <span class="mdc-list-item__text">Roles and Rights</span>
+  </a>
+
+  <hr class="mdc-list-divider">
+  <h6 class="mdc-list-group__subheader">Gamification</h6>
+  <a class="mdc-list-item" href="#">
+    <mwc-icon class="mdc-list-item__graphic">people</mwc-icon>
+    <span class="mdc-list-item__text">Leaderboards</span>
+  </a>
+</nav>
         </div>
         <div slot="appContent">
           <input type="button" @click="${this.openDrawer}" value="Open drawer">
