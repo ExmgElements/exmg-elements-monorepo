1dacf75c704ee498637715ccc76ced121f817cee
pgoc <pawel.goc@amsterdam-standard.pl>
2019-05-15 09:21:19 +0200

    EXM-129 Column manager is hosted by material/menu instead paper-dialog

--- src/table/exmg-grid-setting-selection-list.ts
+++ src/table/exmg-grid-setting-selection-list.ts
@@ -1,24 +1,17 @@
 import {customElement, html, LitElement, property} from 'lit-element';
 import {repeat} from 'lit-html/directives/repeat';
-import '@polymer/paper-dialog/paper-dialog.js';
+
 import '@material/mwc-icon';
-import './exmg-grid-setting';
-import {style} from './exmg-grid-setting-selection-list-styles';
-import {MDCList} from '@material/list';
 import '@material/mwc-checkbox/mwc-checkbox';
+import {MDCMenu} from '@material/menu';
+import {DefaultFocusState} from '@material/menu/constants';
+import {Corner} from '@material/menu-surface/constants';
+
 import {SettingSelectionListItem} from './types/exmg-grid-toolbar-types';
+import {style} from './exmg-grid-setting-selection-list-styles';
 
 @customElement('exmg-grid-setting-selection-list')
 export class ExmgGridSettingSelectionList extends LitElement {
-  @property({type: Boolean})
-  opened: boolean = false;
-
-  @property({type: Boolean, attribute: 'no-cancel-on-outside-click'})
-  noCancelOnOutsideClick: boolean = false;
-
-  @property({type: String, attribute: 'dialog-title'})
-  dialogTitle: string = '';
-
   @property({type: String})
   tooltip: string = '';
 
@@ -28,18 +21,25 @@ export class ExmgGridSettingSelectionList extends LitElement {
   @property({type: Object})
   settingData: SettingSelectionListItem[] = [];
 
-  async handleListAction(e: CustomEvent) {
+  private menu?: MDCMenu;
+
+  private toggleMenuOpenState() {
+    this.menu!.open = !this.menu!.open;
+  }
+
+  private handleListAction(e: CustomEvent) {
+    this.blockEventPropagation(e);
     const index = e.detail.index;
     this.settingData[index].selected = !this.settingData[index].selected;
-    await this.requestUpdate();
+    this.settingData = [...this.settingData];
 
-    this.dispatchEvent(new CustomEvent(
+    this.dispatchEvent(new CustomEvent<{value: SettingSelectionListItem[]}>(
       'exmg-grid-setting-changed',
       {
         bubbles: true,
         composed: true,
         detail: {
-          value: this.settingData,
+          value: [...this.settingData],
         },
       }
     ));
@@ -49,32 +49,53 @@ export class ExmgGridSettingSelectionList extends LitElement {
     style,
   ];
 
-  firstUpdated(changedProperties: any) {
-    super.firstUpdated(changedProperties);
-    new MDCList(this.shadowRoot!.querySelector('.mdc-list')!);
+  protected async firstUpdated(): Promise<void> {
+    await this.updateComplete;
+    const menuElement = this.shadowRoot!.querySelector('.mdc-menu');
+    if (menuElement) {
+      this.menu = new MDCMenu(menuElement)!;
+      this.menu.setDefaultFocusState(DefaultFocusState.FIRST_ITEM);
+      this.menu.setIsHoisted(false);
+      this.menu.setAnchorCorner(Corner.BOTTOM_RIGHT);
+    }
+
+  }
+
+  private blockEventPropagation(e: Event) {
+    // by preventing events menu remains open after selecting single option - we can do multiple selection
+    e.stopPropagation();
   }
 
   render() {
     return html`
-      <exmg-grid-setting
-        class="setting"
-        tooltip="${this.tooltip}"
-        icon="${this.icon}"
-      >
-        <h2>${this.dialogTitle}</h2>
-        <ul class="mdc-list mdc-list--dense" @MDCList:action="${this.handleListAction}">
-          ${repeat(
-            this.settingData,
-          (item) => item.id,
-          item => html`
-            <li class="mdc-list-item">
-              <mwc-checkbox class="mdc-list-item__graphic" ?checked="${item.selected}"></mwc-checkbox>
-              <span class="mdc-list-item__text">${item.title}</span>
-            </li>
-          `
-          )}
-        </ul>
-      </exmg-grid-setting>
+      <exmg-button
+        @click="${this.toggleMenuOpenState}"
+        class="mdc-icon-button material-icons action"
+        title="${this.tooltip}"
+        data-mdc-ripple-is-unbounded="true">
+        <mwc-icon>${this.icon}</mwc-icon>
+      </exmg-button>
+      <div class="mdc-menu-surface--anchor">
+        <div class="mdc-menu mdc-menu-surface" @MDCMenu:selected="${this.blockEventPropagation}">
+          <ul
+            class="mdc-list mdc-list--dense"
+            role="menu"
+            @MDCList:action="${this.handleListAction}"
+            @click="${this.blockEventPropagation}"
+          >
+            ${repeat(
+              this.settingData,
+            (item) => item.id,
+            item => html`
+              <li class="mdc-list-item">
+                <mwc-checkbox class="mdc-list-item__graphic" ?checked="${item.selected}"></mwc-checkbox>
+                <span class="mdc-list-item__text">${item.title}</span>
+              </li>
+            `
+            )}
+          </ul>
+        </div>
+      </div>
     `;
   }
 }
