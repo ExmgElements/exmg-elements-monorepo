fc84be28d6e2b6e44d074ade0ec8440a9a5dd25e
pgoc <pawel.goc@amsterdam-standard.pl>
2019-05-24 00:33:10 +0200

    updated package.json files

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 github_org=ExmgElements
+###### repositories names to migrate ###########
+###############################################
 #my_repos="exmg-paper-token-input exmg-dialogs exmg-button exmg-paper-combobox"
 #my_repos="exmg-form exmg-copy-to-clipboard exmg-cms-styles exmg-markdown-editor exmg-radio-group exmg-sortable exmg-cli"
 #my_repos="exmg-paper-sidemenu exmg-swagger-client exmg-snackbars exmg-tslint exmg-date-format exmg-paper-datatable"
@@ -7,12 +9,5 @@ github_org=ExmgElements
 src_dir=packages
 git checkout -b monorepo-migration
 for repo in $(echo $my_repos); do
-  #mkdir $src_dir/$repo
   git subtree add -P  $src_dir/$repo git@github.com:$github_org/$repo.git master
-#  git remote add $repo git@github.com:$github_org/$repo.git
-#  git fetch $repo
-#  git read-tree --prefix=$src_dir/$repo -u $repo/master
-#  git add $src_dir/$repo
-#  git commit -m "Migrated $repo to $src_dir/$repo"
 done
-#git push -u origin HEAD
