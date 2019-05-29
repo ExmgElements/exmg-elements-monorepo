562ece6e0cf2091c49adcf256bb67cbee25fb74b
Mark Smits <>
2019-05-29 10:19:05 +0200

    Cleanup the elements that should not be inside the monorepo

--- packages/exmg-cms-styles/demo/tabs.d.ts
+++
@@ -1,8 +0,0 @@
-import { LitElement } from 'lit-element';
-import '@exmg/exmg-paper-card/exmg-paper-card.js';
-import '@polymer/paper-tabs/paper-tabs';
-import '@polymer/paper-tabs/paper-tab';
-export declare class TabsDemo extends LitElement {
-    static styles: import("lit-element").CSSResult[];
-    render(): import("lit-element").TemplateResult;
-}
--- packages/exmg-cms-styles/exmg-cms-page-styles.d.ts
+++
@@ -1,5 +0,0 @@
-import '@polymer/iron-flex-layout/iron-flex-layout.js';
-import '@polymer/paper-styles/typography.js';
-import '@polymer/paper-styles/color.js';
-import '@polymer/paper-styles/shadow.js';
-export declare const sharedPageStyles: import("lit-element").CSSResult;
