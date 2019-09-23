6f08bad5717e9673e6303e02d8b2743feae7893a
Rodrigue Couanon <rodrigue@exmachina.nl>
2019-09-23 12:53:38 +0200

    ExmgForm 2.0.5

--- packages/exmg-form/test/utils.ts
+++ packages/exmg-form/test/utils.ts
@@ -2,9 +2,10 @@ import {LitElement} from 'lit-element';
 
 export const promisifyFlush = (flush: Function) => () => new Promise(resolve => flush(resolve));
 
-const onEvent: (eventName: string) => (element: LitElement, shouldResolve: boolean) => Promise<any> = (
-  eventName: string,
-) => (element: LitElement, shouldResolve: boolean) =>
+const onEvent: (eventName: string) => (element: LitElement, shouldResolve: boolean) => Promise<any> = (eventName: string) => (
+  element: LitElement,
+  shouldResolve: boolean,
+) =>
   new Promise((resolve, reject) => {
     console.log('Promise');
     element.addEventListener(eventName, (event: Event) => (shouldResolve ? resolve(event) : reject(event)));
