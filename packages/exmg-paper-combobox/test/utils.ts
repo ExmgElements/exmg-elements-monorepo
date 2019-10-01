4f257a94f358c271ac0653cc7161acc7e44f4396
Rodrigue Couanon <rodrigue@exmachina.nl>
2019-10-01 15:55:46 +0200

    Bumps NPM Version

--- packages/exmg-paper-combobox/test/utils.ts
+++ packages/exmg-paper-combobox/test/utils.ts
@@ -2,9 +2,7 @@ import {LitElement} from 'lit-element';
 
 export const promisifyFlush = (flush: Function) => () => new Promise(resolve => flush(resolve));
 
-const onEvent: (eventName: string) => (element: LitElement) => Promise<any> = (eventName: string) => (
-  element: LitElement,
-) =>
+const onEvent: (eventName: string) => (element: LitElement) => Promise<any> = (eventName: string) => (element: LitElement) =>
   new Promise(resolve => {
     element.addEventListener(eventName, (event: Event) => resolve(event));
   });
