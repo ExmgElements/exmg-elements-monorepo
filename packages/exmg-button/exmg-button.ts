8be463a56a35483a15a23733549c1e7244f4352d
Yigit Can Parilti <yigitcanparilti@gmail.com>
2020-04-09 15:57:50 +0200

    Fix prettier linting

--- packages/exmg-button/exmg-button.ts
+++ packages/exmg-button/exmg-button.ts
@@ -1,18 +1,17 @@
-import { html, property, customElement } from 'lit-element';
-import { classMap } from 'lit-html/directives/class-map.js';
-import { ButtonBase } from '@material/mwc-button/mwc-button-base';
-import { style } from '@material/mwc-button/mwc-button-css';
-import { style as newStyles } from './styles/exmg-button-styles';
-import { ripple } from '@material/mwc-ripple/ripple-directive.js';
+import {html, property, customElement} from 'lit-element';
+import {classMap} from 'lit-html/directives/class-map.js';
+import {ButtonBase} from '@material/mwc-button/mwc-button-base';
+import {style} from '@material/mwc-button/mwc-button-css';
+import {style as newStyles} from './styles/exmg-button-styles';
+import {ripple} from '@material/mwc-ripple/ripple-directive.js';
 import './exmg-spinner';
 
 @customElement('exmg-button')
 export class ExmgButton extends ButtonBase {
-
-  @property({ type: Boolean })
+  @property({type: Boolean})
   public loading = false;
 
-  @property({ type: Number })
+  @property({type: Number})
   public progress = 0;
 
   static styles = [style, newStyles];
@@ -31,7 +30,7 @@ export class ExmgButton extends ButtonBase {
     const mdcButtonIcon = html`
       <span class="material-icons mdc-button__icon">${this.icon}</span>
     `;
-    const buttonRipple = ripple({ unbounded: false });
+    const buttonRipple = ripple({unbounded: false});
     return html`
       <button
         id="button"
@@ -48,10 +47,10 @@ export class ExmgButton extends ButtonBase {
           <slot></slot>
         </span>
         ${this.loading
-        ? html`
+          ? html`
               <exmg-button-spinner active></exmg-button-spinner>
             `
-        : ''}
+          : ''}
         <div class="progress-indicator-holder">
           <div style="width:${this.progress}%" class="progress-indicator"></div>
         </div>
