5322073ff435889de6d5203711cd7b07d42fff9b
Mark Smits <mark@exmachina.nl>
2020-11-25 16:35:56 +0100

    fix some issues

---
+++ packages/exmg-cli/src/gen-docs/bin/gen-docs.js
@@ -0,0 +1,71 @@
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
+const { doFixGenDocs } = require('../index');
+const options = [
+    {
+        name: 'project-dir',
+        alias: 'p',
+        type: String,
+        description: 'Path to the project',
+        defaultOption: true,
+        defaultValue: '',
+    },
+    {
+        name: 'docs-dir',
+        alias: 'd',
+        type: String,
+        description: 'Relative path to project-dir where ',
+    },
+    {
+        name: 'apply',
+        alias: 'a',
+        type: Boolean,
+        description: 'If false then will print list of files which will be affected. If true then print and update files',
+        defaultValue: false,
+    },
+    {
+        name: 'help',
+        alias: 'h',
+        type: Boolean,
+        description: 'Print this message.',
+    },
+    {
+        name: 'verbose',
+        alias: 'v',
+        type: Boolean,
+        description: 'Print useful information',
+    },
+];
+const { projectDir, docsDir, apply, help, verbose } = commandLineArgs(options, { camelCase: true });
+function printUsage() {
+    const sections = [
+        {
+            header: 'gen-docs',
+            content: 'Replace all absolute paths from "project-dir/docs-dir/*.html"',
+        },
+        {
+            header: 'Options',
+            optionList: options,
+        },
+    ];
+    console.log(commandLineUsage(sections));
+}
+if (help) {
+    printUsage();
+    process.exit(0);
+}
+if (!(projectDir && docsDir)) {
+    console.error('Must provide a project-dir and docs-dir!');
+    printUsage();
+    process.exit(-1);
+}
+if (verbose || !apply) {
+    console.log('ARGUMENTS:');
+    const inputOptions = { projectDir, docsDir, apply, help, verbose };
+    console.log(JSON.stringify(inputOptions, (_, v) => (typeof v === 'undefined' ? null : v), 2));
+}
+doFixGenDocs(projectDir, docsDir, apply, verbose).catch((err) => {
+    console.error(err);
+    process.exit(-1);
+});
+export {};
---
+++ packages/exmg-cli/src/sass-render/watcher.js
@@ -0,0 +1,45 @@
+const watch = require('node-watch');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
+const watchOptions = {
+  recursive: true,
+  filter: path => {
+    console.log('p', path);
+    if (path.indexOf('node_modules') > -1) {
+      return false;
+    }
+    if (path.indexOf('scss') === -1) {
+      return false;
+    }
+    return /.(?:scss)$/.test(path);
+  }
+};
+
+let updating = false;
+
+async function addToQueue(fileName) {
+  if (updating) {
+    return;
+  }
+  console.log(`saw change to ${fileName}`);
+  updating = true;
+  console.log('building styles');
+  const execPromise = exec(`node ./node_modules/@exmg/exmg-cli/src/sass-render/bin/sass-render.js -s "${fileName}"`);
+  try {
+    console.log(`node ./node_modules/@exmg/exmg-cli/src/sass-render/bin/sass-render.js -s "${fileName}"`);
+    const { stdout } = await execPromise;
+    console.log(stdout);
+  } catch ({ stdout, stderr }) {
+    console.log(stdout);
+    console.log('ERROR:', stderr);
+  }
+  console.log('watcher build complete!');
+  updating = false;
+}
+
+watch('src', watchOptions, (_event, fileName) => {
+  addToQueue(fileName);
+});
+
+console.log('watcher started!');
