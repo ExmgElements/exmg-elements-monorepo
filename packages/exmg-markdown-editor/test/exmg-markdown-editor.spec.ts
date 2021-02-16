f818d56ba77f652fd7a0de3f437ce1d444dcb50b
Mark Smits <mark@exmachina.nl>
2021-02-16 15:16:31 +0100

    Fix for eslint issues

--- packages/exmg-markdown-editor/test/exmg-markdown-editor.spec.ts
+++ packages/exmg-markdown-editor/test/exmg-markdown-editor.spec.ts
@@ -1,27 +1,27 @@
 import {EditorElement} from '../exmg-markdown-editor';
 import {promisifyFlush, onValueChange, onExmgMarkdownEditorFullscreen} from './utils';
 
-declare const fixture: <T extends HTMLElement = HTMLElement>(id: string, model?: object) => T;
-declare const flush: (cb?: Function) => void;
+declare const fixture: <T extends HTMLElement = HTMLElement>(id: string, model?: any) => T;
+declare const flush: (cb?: unknown) => void;
 
 const {assert} = chai;
 
-suite('<exmg-markdown-editor>', function() {
+suite('<exmg-markdown-editor>', function () {
   let element: EditorElement;
   const flushCompleted = promisifyFlush(flush);
 
-  suite('base usage', function() {
+  suite('base usage', function () {
     setup(() => {
       element = fixture('BasicTestFixture');
     });
 
-    test('check if default toolbar is rendered correctly', async function() {
+    test('check if default toolbar is rendered correctly', async function () {
       await flushCompleted();
       const elements = element.shadowRoot!.querySelectorAll('#toolbar a');
       assert.equal(elements.length, 18, 'By default should be 18 items in tool bar');
     });
 
-    test('check attribute split-view is reflected', async function() {
+    test('check attribute split-view is reflected', async function () {
       await flushCompleted();
 
       const getMarkdownElementComputedStyle = () => window.getComputedStyle(element.querySelector('marked-element')!);
@@ -36,7 +36,7 @@ suite('<exmg-markdown-editor>', function() {
       assert.isFalse(element.hasAttribute('split-view'), 'Attribute split view is not present when turn off');
     });
 
-    test('check attribute fullscreen is reflected', async function() {
+    test('check attribute fullscreen is reflected', async function () {
       await flushCompleted();
       assert.isFalse(element.hasAttribute('fullscreen'), 'fullscreen attr is not reflected by default');
       assert.isFalse(element.fullscreen, 'fullscreen prop should be turn off by default');
@@ -46,7 +46,7 @@ suite('<exmg-markdown-editor>', function() {
       assert.isTrue(element.hasAttribute('fullscreen'), 'Attribute fullscreen should be reflected');
     });
 
-    test('event exmg-markdown-editor-fullscreen should be triggered', async function() {
+    test('event exmg-markdown-editor-fullscreen should be triggered', async function () {
       await flushCompleted();
       element.fullscreen = true;
       const eventOn = await onExmgMarkdownEditorFullscreen(element);
@@ -59,7 +59,7 @@ suite('<exmg-markdown-editor>', function() {
       assert.isFalse(eventOff.detail, 'Should be triggered event when fullscreen off');
     });
 
-    test('event value-change should be triggered', async function() {
+    test('event value-change should be triggered', async function () {
       element.markdown = '__TEST__';
       const secondEvent = await onValueChange(element);
       assert.instanceOf(secondEvent, CustomEvent);
@@ -67,18 +67,18 @@ suite('<exmg-markdown-editor>', function() {
     });
   });
 
-  suite('with custom toolbar and autofocus', function() {
+  suite('with custom toolbar and autofocus', function () {
     setup(() => {
       element = fixture('ExmgToolBarTestFixture');
     });
 
-    test('element has 2 items in toolbar', async function() {
+    test('element has 2 items in toolbar', async function () {
       await flushCompleted();
       const elements = element.shadowRoot!.querySelectorAll('#toolbar a');
       assert.equal(elements.length, 2, 'There should be 2 items in toolbar');
     });
 
-    test('element editor is focused', async function() {
+    test('element editor is focused', async function () {
       await flushCompleted();
       const editorTextArea = element.shadowRoot!.querySelector('#editor textarea');
       assert.instanceOf(editorTextArea, HTMLTextAreaElement, 'Text area exists');
@@ -86,7 +86,7 @@ suite('<exmg-markdown-editor>', function() {
     });
   });
 
-  suite('Raw text should be parsed to html', function() {
+  suite('Raw text should be parsed to html', function () {
     let getMarkdownElementText: (selector: string) => string | undefined;
     setup(() => {
       element = fixture('ExmgStylingTextFixture');
@@ -96,7 +96,7 @@ suite('<exmg-markdown-editor>', function() {
         (markdownBody.querySelector<HTMLElement>(selector) || {innerText: undefined})!.innerText;
     });
 
-    test('Should parse headers', async function() {
+    test('Should parse headers', async function () {
       await flushCompleted();
 
       assert.equal(getMarkdownElementText('h1#h1'), 'H1', '#H1 is present as <h1>');
@@ -107,7 +107,7 @@ suite('<exmg-markdown-editor>', function() {
       assert.equal(getMarkdownElementText('h6#h6'), 'H6', '######H6 is present as <h6>');
     });
 
-    test('Should parse text', async function() {
+    test('Should parse text', async function () {
       await flushCompleted();
 
       assert.equal(getMarkdownElementText('p strong'), 'ipsum', '**ipsum** is present as <strong>');
@@ -115,20 +115,20 @@ suite('<exmg-markdown-editor>', function() {
       assert.equal(getMarkdownElementText('p em'), 'eiusmod', '*eiusmod* is present as <em>');
     });
 
-    test('Should parse table', async function() {
+    test('Should parse table', async function () {
       await flushCompleted();
 
       assert.equal(getMarkdownElementText('table th:nth-child(1)'), 'User Name', '|User name| is present as <table>');
       assert.equal(getMarkdownElementText('table th:nth-child(2)'), 'Email', '|Email| is present as <table>');
     });
 
-    test('Should parse quote', async function() {
+    test('Should parse quote', async function () {
       await flushCompleted();
 
       assert.include(getMarkdownElementText('blockquote')!, 'Lorem ipsum dolor', '> is present as <blockquote>');
     });
 
-    test('Should parse list', async function() {
+    test('Should parse list', async function () {
       await flushCompleted();
 
       assert.equal(getMarkdownElementText('ul li:nth-child(1)'), 'List option 1', '* List option 1 is present as <ul><li>');
@@ -137,20 +137,20 @@ suite('<exmg-markdown-editor>', function() {
       assert.equal(getMarkdownElementText('ol li:nth-child(2)'), 'List option 2', '2. List option 2 is present as <ol><li>');
     });
 
-    test('Should parse hr', async function() {
+    test('Should parse hr', async function () {
       await flushCompleted();
 
       assert.equal(getMarkdownElementText('hr'), '', '--- 1 is present as <hr>');
     });
 
-    test('Should parse code', async function() {
+    test('Should parse code', async function () {
       await flushCompleted();
 
       assert.equal(getMarkdownElementText('p code'), 'Inline code', '`` is present as <code>');
       assert.equal(getMarkdownElementText('pre code'), 'Block\ncode\n', '``` is present as <pre><code>');
     });
 
-    test('Should parse image', async function() {
+    test('Should parse image', async function () {
       await flushCompleted();
       const img = element.querySelector<HTMLImageElement>('.markdown-body img')!;
 
