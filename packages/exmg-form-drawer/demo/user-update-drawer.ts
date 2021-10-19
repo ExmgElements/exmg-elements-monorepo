06033d2b5eb32ac63dc00c4b8b9ae3164958c4b4
Mark Smits <mark@exmachina.nl>
2021-10-19 15:50:22 +0200

    Updated form-drawer

--- packages/exmg-form-drawer/demo/user-update-drawer.ts
+++ packages/exmg-form-drawer/demo/user-update-drawer.ts
@@ -45,6 +45,7 @@ export class UserUpdateDrawer extends FormDrawerBase<UserData> {
 
       target.done();
     } catch (error) {
+      // @ts-ignore
       target.error(`${error.message}`);
     }
   }
