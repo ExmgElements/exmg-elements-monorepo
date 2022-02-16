e6fc983e12bfe2b38fcc507bfdc5272e49f63046
Mark Smits <mark@exmachina.nl>
2022-02-16 16:04:53 +0100

    lint fixes

--- packages/exmg-paper-token-input/test/utils.ts
+++ packages/exmg-paper-token-input/test/utils.ts
@@ -1,9 +1,9 @@
 import {LitElement} from 'lit';
 
-export const promisifyFlush = (flush: any) => () => new Promise(resolve => flush(resolve));
+export const promisifyFlush = (flush: any) => () => new Promise((resolve) => flush(resolve));
 
 const onEvent: (eventName: string) => (element: LitElement) => Promise<any> = (eventName: string) => (element: LitElement) =>
-  new Promise(resolve => {
+  new Promise((resolve) => {
     element.addEventListener(eventName, (event: Event) => resolve(event));
   });
 
